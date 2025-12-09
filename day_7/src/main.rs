bfa71acc2e8cd3dbdce50564bfb55e6887f47f53
Sam Forbes <samuelforbes@live.com>
2025-12-09 20:57:57 +0000

    day 7 part one

---
+++ day_7/src/main.rs
@@ -0,0 +1,62 @@
+use std::collections::HashSet;
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+
+    let mut start = Instant::now();
+    println!("{} - {:?}", part_one(&input), start.elapsed());
+    //
+    // start = Instant::now();
+    // println!("{} - {:?}", part_two(&input), start.elapsed());
+}
+
+fn part_one(input: &str) -> u32 {
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut beams: HashSet<usize> = HashSet::new();
+    beams.insert(grid[0].iter().position(|c| *c == 'S').unwrap());
+
+    let mut split_count = 0;
+
+    for row in grid {
+        let mut new_beams = HashSet::new();
+        for beam in &mut beams.iter() {
+            if row[*beam] == '^' {
+                if *beam != 0 {
+                    new_beams.insert(beam - 1);
+                }
+                if *beam != row.len() - 1 {
+                    new_beams.insert(beam + 1);
+                }
+                split_count += 1;
+            } else {
+                new_beams.insert(*beam);
+            }
+        }
+        beams = new_beams;
+    }
+
+    split_count
+}
+
+#[test]
+fn small_input(){
+    let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+    assert_eq!(21, part_one(input));
+}

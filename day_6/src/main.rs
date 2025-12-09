80c809ca39ebfd27bf2b10725002cc7255ea3967
Sam Forbes <samuelforbes@live.com>
2025-12-09 20:38:39 +0000

    day 6 part two progress

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     // println!("{} - {:?}", part_two(&input), start.elapsed());
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Action {
     Add,
     Multiply
@@ -23,6 +23,13 @@ impl Action {
             '+' => Action::Add,
             '*' => Action::Multiply,
             _ => unreachable!("Unknown action {c}")
+        }
+    }
+
+    fn base_value(&self) -> u64 {
+        match self {
+            Action::Add => 0,
+            Action::Multiply => 1,
         }
     }
 }
@@ -45,6 +52,50 @@ fn part_one(input: &str) -> u64 {
     total
 }
 
+fn part_two(input: &str) -> u64 {
+    let grid: Vec<Vec<Vec<char>>> = input.lines().map(|line| line.split_ascii_whitespace().map(|value| value.chars().collect()).collect()).collect();
+
+    let mut total = 0;
+
+    for i in 0..grid[0].len() {
+        let action = Action::from(grid.last().unwrap()[i][0]);
+
+        let mut calculation_total = action.base_value();
+        let maximum_instruction_length = find_maximum_instruction_length(&grid, i);
+        println!("{maximum_instruction_length}");
+
+        for j in (0..(maximum_instruction_length - 1)).rev() {
+            println!("{j}");
+            let mut value_parts = Vec::new();
+            for k in 0..grid.len() - 1 {
+                println!("{:?}", grid[k][i]);
+                if j < grid[k][i].len(){
+                    value_parts.push(grid[k][i][j]);
+                }
+            }
+            let value: u64 = value_parts.iter().collect::<String>().parse().unwrap();
+            if action == Action::Add { calculation_total += value } else { calculation_total *= value};
+        }
+
+        total += calculation_total;
+    }
+
+    total
+}
+
+fn find_maximum_instruction_length(grid: &Vec<Vec<Vec<char>>>, i: usize) -> usize {
+    let mut maximum_instruction_length: usize = 0;
+
+    for j in 0..grid.len() - 1 {
+        let instruction_length = grid[j][i].len();
+        if instruction_length > maximum_instruction_length {
+            maximum_instruction_length = instruction_length;
+        }
+    }
+
+    maximum_instruction_length
+}
+
 
 #[test]
 fn small_input(){
@@ -53,6 +104,7 @@ fn small_input(){
   6 98  215 314
 *   +   *   + ";
 
-    assert_eq!(4277556, part_one(input));
+    assert_eq!(4_277_556, part_one(input));
+    assert_eq!(3_263_827, part_two(input));
 }
 
